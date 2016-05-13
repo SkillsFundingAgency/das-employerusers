c6c3e8d9aff991084dfe4b03b438a9c0444e5195
Simon Bull <simon.bull@taurumconsulting.com>
2016-05-13 11:06:56 +0100

    Add skeleton project

---
+++ src/SFA.DAS.EmployerUsers.Web.UnitTests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SFA.DAS.EmployerUsers.Web.UnitTests
+{
+    public class Class1
+    {
+        [Test]
+        public void IsTrueTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
