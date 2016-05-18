700c8cba79531da9a83bdadb57dff582562d6b50
Simon Bull <simon.bull@taurumconsulting.com>
2016-05-18 10:09:28 +0100

    Implement IsUserActiveQueryHandler

--- src/SFA.DAS.EmployerUsers.Data/User/IUserRepository.cs
+++ src/SFA.DAS.EmployerUsers.Data/User/IUserRepository.cs
@@ -1,7 +1,10 @@
-﻿namespace SFA.DAS.EmployerUsers.Data.User
+﻿using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerUsers.Data.User
 {
     public interface IUserRepository
     {
+        Task<Domain.User> GetById(string id);
         void Create(Domain.User registerUser);
     }
 }
--- src/SFA.DAS.EmployerUsers.Domain/User.cs
+++ src/SFA.DAS.EmployerUsers.Domain/User.cs
@@ -7,5 +7,6 @@
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public bool IsActive { get; set; }
     }
 }
