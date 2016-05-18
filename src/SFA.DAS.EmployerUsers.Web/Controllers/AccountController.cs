abf72e71a2b4d9bc1f7c7d5b44bb553b0013c219
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-05-18 14:59:16 +0100

    Updated Controller and View for Register so that details are re-populated on error and a message is displayed saying that there is an error

--- src/SFA.DAS.EmployerUsers.Web/Controllers/AccountController.cs
+++ src/SFA.DAS.EmployerUsers.Web/Controllers/AccountController.cs
@@ -17,7 +17,7 @@ namespace SFA.DAS.EmployerUsers.Web.Controllers
         [HttpGet]
         public async Task<ActionResult> Register()
         {
-            return await Task.Run<ActionResult>(() => View(true));
+            return await Task.Run<ActionResult>(() => View(new RegisterViewModel {Valid = true}));
         }
 
         [HttpPost]
@@ -32,7 +32,11 @@ namespace SFA.DAS.EmployerUsers.Web.Controllers
                     return RedirectToAction("Confirm");
                 }
 
-                return View("Register",false);
+                model.ConfirmPassword = string.Empty;
+                model.Password = string.Empty;
+                model.Valid = false;
+
+                return View("Register",model);
             });
         }
 
