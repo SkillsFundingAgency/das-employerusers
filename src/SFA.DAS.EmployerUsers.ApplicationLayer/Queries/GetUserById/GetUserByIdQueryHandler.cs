0dff559c927c03378c6c2034f7c781c94e7b2134
Simon Bull <simon.bull@taurumconsulting.com>
2016-05-18 10:45:58 +0100

    Implement GetUserByIdQueryHandler

---
+++ src/SFA.DAS.EmployerUsers.ApplicationLayer/Queries/GetUserById/GetUserByIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.EmployerUsers.Data.User;
+using SFA.DAS.EmployerUsers.Domain;
+
+namespace SFA.DAS.EmployerUsers.ApplicationLayer.Queries.GetUserById
+{
+    public class GetUserByIdQueryHandler : IAsyncRequestHandler<GetUserByIdQuery, User>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUserByIdQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public Task<User> Handle(GetUserByIdQuery message)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            return _userRepository.GetById(message.UserId);
+        }
+    }
+}
