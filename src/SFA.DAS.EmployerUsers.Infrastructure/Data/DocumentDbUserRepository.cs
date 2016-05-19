2e01ac4bfa20efda4bae2170b6bdf13d6f9cbb2d
Simon Bull <simon.bull@taurumconsulting.com>
2016-05-19 16:08:09 +0100

    Implement DocumentDb user repo

--- src/SFA.DAS.EmployerUsers.Infrastructure/Data/DocumentDbUserRepository.cs
+++ src/SFA.DAS.EmployerUsers.Infrastructure/Data/DocumentDbUserRepository.cs
@@ -1,23 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net;
 using System.Threading.Tasks;
-using SFA.DAS.EmployerUsers.Domain;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using SFA.DAS.Configuration;
 using SFA.DAS.EmployerUsers.Domain.Data;
+using SFA.DAS.EmployerUsers.Infrastructure.Configuration;
+using User = SFA.DAS.EmployerUsers.Domain.User;
 
 namespace SFA.DAS.EmployerUsers.Infrastructure.Data
 {
     public class DocumentDbUserRepository : IUserRepository
     {
-        public Task<User> GetById(string id)
+        private const string DatabaseName = "EmployerUsers";
+        private const string CollectionName = "User";
+
+        private readonly DocumentClient _client;
+
+        public DocumentDbUserRepository(IConfigurationService configurationService)
         {
-            return Task.FromResult<User>(null);
+            var cfgTask = configurationService.Get<EmployerUsersConfiguration>();
+            cfgTask.Wait();
+            var configuration = cfgTask.Result;
+
+            _client = new DocumentClient(new Uri(configuration.DataStorage.DocumentDbUri), configuration.DataStorage.DocumentDbAccessToken);
         }
 
-        public Task Create(User registerUser)
+        public async Task<User> GetById(string id)
         {
-            return Task.FromResult<object>(null);
+            try
+            {
+                var documentUri = UriFactory.CreateDocumentUri(DatabaseName, CollectionName, id);
+                var document = await _client.ReadDocumentAsync(documentUri);
+
+                DocumentDbUser documentDbUser = (dynamic)document.Resource;
+                return documentDbUser.ToDomainUser();
+            }
+            catch (DocumentClientException de)
+            {
+                if (de.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return null;
+                }
+                throw;
+            }
+        }
+
+        public async Task Create(User registerUser)
+        {
+            registerUser.Id = Guid.NewGuid().ToString();
+
+            var collectionId = UriFactory.CreateDocumentCollectionUri(DatabaseName, CollectionName);
+            var documentDbUser = DocumentDbUser.FromDomainUser(registerUser);
+            await _client.CreateDocumentAsync(collectionId, documentDbUser, null, true);
         }
     }
 }
