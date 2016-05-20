46f947c665325b39c531cee8a928700f8a9ce80b
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-05-20 09:55:36 +0100

    Added in code to wire up the orchestrator with the mediator for entering an access code

---
+++ src/SFA.DAS.EmployerUsers.Web/Models/AccessCodeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.EmployerUsers.Web.Models
+{
+    public class AccessCodeViewModel
+    {
+        public string AccessCode { get; set; }
+        public string UserId { get; set; }
+    }
+}
