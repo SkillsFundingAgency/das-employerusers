08e76aaffb350072c20240a89d17b7b7678517ae
Simon Bull <simon.bull@taurumconsulting.com>
2016-05-24 09:50:34 +0100

    Stop trying to install certificates to Trusted CA store

--- src/SFA.DAS.EmployerUsers.Web/App_Start/Startup.RelyingParty.cs
+++ src/SFA.DAS.EmployerUsers.Web/App_Start/Startup.RelyingParty.cs
@@ -24,7 +24,7 @@ namespace SFA.DAS.EmployerUsers.Web
 
             app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
             {
-                Authority = configuration.IdentityProviderUrl, //"/identity",
+                Authority = configuration.IdentityProviderUrl,,
 
                 ClientId = "idp",
                 Scope = "openid profile",
