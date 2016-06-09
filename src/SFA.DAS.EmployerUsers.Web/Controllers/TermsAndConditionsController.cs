8ff606837cb2ce58bb6685d81ca2fcba5317e005
Simon Bull <simon.bull@taurumconsulting.com>
2016-06-09 14:08:42 +0100

    Add terms and conditions page and link from footer

---
+++ src/SFA.DAS.EmployerUsers.Web/Controllers/TermsAndConditionsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SFA.DAS.EmployerUsers.Web.Controllers
+{
+    public class TermsAndConditionsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
