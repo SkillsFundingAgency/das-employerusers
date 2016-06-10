7e14b0665f74401e4df7578ec32d9c6277b27250
ijrussell <i.j.russell@tesco.net>
2016-06-10 11:30:15 +0100

    Handle existing user and new user

---
+++ src/SFA.DAS.EmployerUsers.Application/Queries/GetUserByEmailAddress/GetUserByEmailAddressQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using SFA.DAS.EmployerUsers.Domain;
+
+namespace SFA.DAS.EmployerUsers.Application.Queries.GetUserByEmailAddress
+{
+    public class GetUserByEmailAddressQuery : IAsyncRequest<User>
+    {
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ src/SFA.DAS.EmployerUsers.Application/Queries/GetUserByEmailAddress/GetUserByEmailAddressQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.EmployerUsers.Application.Queries.GetUserById;
+using SFA.DAS.EmployerUsers.Domain;
+using SFA.DAS.EmployerUsers.Domain.Data;
+
+namespace SFA.DAS.EmployerUsers.Application.Queries.GetUserByEmailAddress
+{
+    public class GetUserByEmailAddressQueryHandler : IAsyncRequestHandler<GetUserByIdQuery, User>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUserByEmailAddressQueryHandler(IUserRepository userRepository)
+        {
+            if (userRepository == null)
+                throw new ArgumentNullException(nameof(userRepository));
+            _userRepository = userRepository;
+        }
+
+        public Task<User> Handle(GetUserByEmailAddressQuery message)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            return _userRepository.GetByEmailAddress(message.EmailAddress);
+        }
+    }
+}
