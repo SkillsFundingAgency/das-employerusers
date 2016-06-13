aeb09f322193a22c0d308ad33858fcf0dabfc3d4
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-06-13 09:38:10 +0100

    Added resend parameter to the AccountLockedEvent and updated the functionality so that a UnlockCode will be resent. The same code will be used unless it has expired. If it has then a new code will be generated

--- src/SFA.DAS.EmployerUsers.Application/Events/AccountLocked/AccountLockedEvent.cs
+++ src/SFA.DAS.EmployerUsers.Application/Events/AccountLocked/AccountLockedEvent.cs
@@ -6,5 +6,6 @@ namespace SFA.DAS.EmployerUsers.Application.Events.AccountLocked
     public class AccountLockedEvent : IAsyncNotification
     {
         public User User { get; set; }
+        public bool ResendUnlockCode { get; set; }
     }
 }
