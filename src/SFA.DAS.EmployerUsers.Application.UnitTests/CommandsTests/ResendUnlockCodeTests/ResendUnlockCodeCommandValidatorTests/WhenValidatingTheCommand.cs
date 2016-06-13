2aa530e5f4a17cbaaa59982df04389e9c74f601e
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-06-13 16:57:07 +0100

    Added in validator logic for the ResendUnlockCodeCommandValidator

---
+++ src/SFA.DAS.EmployerUsers.Application.UnitTests/CommandsTests/ResendUnlockCodeTests/ResendUnlockCodeCommandValidatorTests/WhenValidatingTheCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SFA.DAS.EmployerUsers.Application.Commands.ResendUnlockCode;
+
+namespace SFA.DAS.EmployerUsers.Application.UnitTests.CommandsTests.ResendUnlockCodeTests.ResendUnlockCodeCommandValidatorTests
+{
+    public class WhenValidatingTheCommand
+    {
+        private ResendUnlockCodeCommandValidator _validator;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _validator = new ResendUnlockCodeCommandValidator();
+        }
+
+        [Test]
+        public void ThenFalseIsReturnedIfTheValuesAreEmpty()
+        {
+            //Act
+            var actual = _validator.Validate(new ResendUnlockCodeCommand());
+
+            //Assert
+            Assert.IsFalse(actual.IsValid());
+        }
+
+        [Test]
+
+        public void ThenTrueIsReturnedIfTheValuesAreNotEmpty()
+        {
+            //act
+            var actual = _validator.Validate(new ResendUnlockCodeCommand {Email = "someEmail"});
+
+            //Assert
+            Assert.IsTrue(actual.IsValid());
+        }
+
+        [Test]
+        public void ThenTheErrorDictionaryIsPopulatedCorrectlyWhenThereIsAnError()
+        {
+            //Act
+            var actual = _validator.Validate(new ResendUnlockCodeCommand());
+
+            //Assert
+            Assert.IsNotNull(actual);
+            Assert.Contains(new KeyValuePair<string,string>("Email", "Please enter email address"), actual.ValidationDictionary);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerUsers.Application/Commands/ResendUnlockCode/ResendUnlockCodeCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFA.DAS.EmployerUsers.Application.Validation;
+
+namespace SFA.DAS.EmployerUsers.Application.Commands.ResendUnlockCode
+{
+    public class ResendUnlockCodeCommandValidator : IValidator<ResendUnlockCodeCommand>
+    {
+        public ValidationResult Validate(ResendUnlockCodeCommand item)
+        {
+            var result = new ValidationResult();
+            if (string.IsNullOrEmpty(item.Email))
+            {
+                result.ValidationDictionary.Add(nameof(item.Email), "Please enter email address");
+            }
+
+            return result;
+        }
+    }
+}
