175b780db8af6b5396ae16971f87317227ec60ea
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-06-13 16:31:08 +0100

    Added in ResendUnlockCodeCommandHandler and tests

---
+++ src/SFA.DAS.EmployerUsers.Application/Commands/ResendUnlockCode/ResendUnlockCodeCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace SFA.DAS.EmployerUsers.Application.Commands.ResendUnlockCode
+{
+    public class ResendUnlockCodeCommand : IAsyncRequest
+    {
+        public string Email { get; set; }
+    }
+}
---
+++ src/SFA.DAS.EmployerUsers.Web/Models/ResendUnlockCodeViewModel.cs
@@ -0,0 +1,7 @@
+namespace SFA.DAS.EmployerUsers.Web.Models
+{
+    public class ResendUnlockCodeViewModel : ViewModelBase
+    {
+        public string Email { get; set; }
+    }
+}
