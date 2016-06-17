bc7848b0e2251ce2482d45890831af3428d3a6c4
ijrussell <i.j.russell@tesco.net>
2016-06-17 15:10:45 +0100

    Plug reset password code into UI

---
+++ src/SFA.DAS.EmployerUsers.Web/Models/RequestPasswordResetViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.EmployerUsers.Web.Models
+{
+    public class RequestPasswordResetViewModel
+    {
+        public string Email { get; set; }
+
+        public bool ResetCodeSent { get; set; }
+
+        public Dictionary<string, string> ErrorDictionary { get; set; }
+    }
+}
