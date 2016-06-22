d66ca05764ae56574d0899e8d0c5ba6f000bc7e0
Mark Gwilliam <github@markgwilliam.com>
2016-06-22 08:28:00 +0100

    added requirement for running as admin

--- src/DevInstall.ps1
+++ src/DevInstall.ps1
@@ -1,3 +1,5 @@
+#Requires -RunAsAdministrator
+
 $certpwd = ConvertTo-SecureString -String password -Force -AsPlainText
 
 Import-PfxCertificate -FilePath localhost.pfx -CertStoreLocation cert://LocalMachine/My -Password $certpwd -Exportable
