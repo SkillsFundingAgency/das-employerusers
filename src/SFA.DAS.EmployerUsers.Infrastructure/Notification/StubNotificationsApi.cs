917d9d325992d8a5f7ca9ad2ec8f2a93a1f7ee1a
Simon Bull <sbulluk@users.noreply.github.com>
2016-11-24 10:31:07 +0000

    Add stubbed notification api

---
+++ src/SFA.DAS.EmployerUsers.Infrastructure/Notification/StubNotificationsApi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SFA.DAS.Notifications.Api.Client;
+using SFA.DAS.Notifications.Api.Types;
+
+namespace SFA.DAS.EmployerUsers.Infrastructure.Notification
+{
+    public class StubNotificationsApi : INotificationsApi
+    {
+        private readonly string _directory;
+
+        public StubNotificationsApi()
+        {
+            _directory = Path.Combine((string)AppDomain.CurrentDomain.GetData("DataDirectory"), "Emails");
+        }
+
+        public async Task SendEmail(Email email)
+        {
+            var id = string.IsNullOrEmpty(email.SystemId) ? Guid.NewGuid().ToString() : email.SystemId;
+            var path = Path.Combine(_directory, id + ".json");
+
+            if (!Directory.Exists(_directory))
+            {
+                Directory.CreateDirectory(_directory);
+            }
+
+            using (var stream = new FileStream(path, FileMode.Create, FileAccess.Write))
+            using (var writer = new StreamWriter(stream))
+            {
+                await writer.WriteAsync(JsonConvert.SerializeObject(email));
+            }
+        }
+    }
+}
