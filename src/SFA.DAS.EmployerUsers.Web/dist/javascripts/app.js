6c615494409db309956283c15c094dee5aeee3e2
James King <hello@jamesmichaelking.com>
2016-12-14 15:49:14 +0000

    Updated layout with the new header

--- src/SFA.DAS.EmployerUsers.Web/dist/javascripts/app.js
+++ src/SFA.DAS.EmployerUsers.Web/dist/javascripts/app.js
@@ -1,1 +1,106 @@
+var sfa = sfa || {};
+    
+sfa.homePage = {
+    init: function () {
+        this.startButton();
+        this.toggleRadios();
+    },
+    startButton: function () {
+        var that = this;
+        $('#submit-button').on('click touchstart', function (e) {
+            var isYesClicked = $('#have-everything').prop('checked'),
+                errorShown = $('body').data('shownError') || false;
+            if (!isYesClicked && !errorShown) {
+                e.preventDefault();
+                that.showError();
+            }
+        });
+    }, 
+    showError: function() {
+        $('.error-message').removeClass("js-hidden").attr("aria-hidden");
+        $('#what-you-need-form').addClass("error");
+        $('body').data('shownError', true);
+    },
+    toggleRadios: function () {
+        var radios = $('input[type=radio][name=everything-you-need]');
+        radios.on('change', function () {
+
+            radios.each(function () {
+                if ($(this).prop('checked')) {
+                    var target = $(this).parent().data("target");
+                    $("#" + target).removeClass("js-hidden").attr("aria-hidden");
+                } else {
+                    var target = $(this).parent().data("target");
+                    $("#" + target).addClass("js-hidden").attr("aria-hidden", "true");
+                }
+            });
+
+        });
+    }
+}
+
+sfa.navigation = {
+    elems: {
+        userNav: $('nav#user-nav > ul'),
+        levyNav: $('ul#global-nav-links')
+    },
+    init: function () {
+        this.setupMenus(this.elems.userNav);
+        this.setupEvents(this.elems.userNav);
+        this.linkSettings();
+    },
+    setupMenus: function (menu) {
+        menu.find('ul').addClass("js-hidden").attr("aria-hidden", "true");
+    },
+    setupEvents: function (menu) {
+        var that = this;
+        menu.find('li.has-sub-menu > a').on('click', function (e) {
+            var $that = $(this);
+            that.toggleMenu($that, $that.next('ul'));
+            e.stopPropagation();
+            e.preventDefault();
+        });
+    },
+    toggleMenu: function (link, subMenu) {
+        var $li = link.parent();
+        if ($li.hasClass("open")) {
+            // Close menu
+            $li.removeClass("open");
+            subMenu.addClass("js-hidden").attr("aria-hidden", "true");
+        } else {
+            // Open menu
+            this.closeAllOpenMenus();         
+            $li.addClass("open");
+            subMenu.removeClass("js-hidden").attr("aria-hidden", "false");
+        }
+    },
+    closeAllOpenMenus: function () {
+        this.elems.userNav.find('li.has-sub-menu.open').removeClass('open').find('ul').addClass("js-hidden").attr("aria-hidden", "true");
+        this.elems.levyNav.find('li.open').removeClass('open').addClass("js-hidden").attr("aria-hidden", "true");
+    },
+    linkSettings: function () {
+        var $settingsLink = $('a#link-settings'),
+            that = this;
+        this.toggleUserMenu();
+        $settingsLink.attr("aria-hidden", "false");
+        $settingsLink.on('click touchstart', function (e) {
+            var target = $(this).attr('href');
+            $(this).toggleClass('open');
+            that.toggleUserMenu();
+            e.preventDefault();
+        });
+    },
+    toggleUserMenu: function () {
+        var $userNavParent = this.elems.userNav.parent();
+        if ($userNavParent.hasClass("close")) {
+            //open it
+            $userNavParent.removeClass("close").attr("aria-hidden", "false");
+        } else {
+            // close it 
+            $userNavParent.addClass("close").attr("aria-hidden", "true");
+        }
+    }
+}
+
+sfa.navigation.init();
 var selectionButtons = new GOVUK.SelectionButtons("label input[type='radio'], label input[type='checkbox']");
