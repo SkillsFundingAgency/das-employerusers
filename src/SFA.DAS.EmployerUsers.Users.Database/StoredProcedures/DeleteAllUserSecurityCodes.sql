419600469ef5b82d4fee395d875be1b1b9dc933b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-12-21 21:45:13 +0000

    Change to database project names
    Changed the database project names to be picked up by the build script

---
+++ src/SFA.DAS.EmployerUsers.Users.Database/StoredProcedures/DeleteAllUserSecurityCodes.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [dbo].[DeleteAllUserSecurityCodes]
+	@Userid varchar(50)
+AS
+	DELETE FROM [UserSecurityCode] WHERE UserId = @UserId
+GO
---
+++ src/SFA.DAS.EmployerUsers.Users.Database/StoredProcedures/DeleteUserPasswordHistory.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [dbo].[DeleteUserPasswordHistory]
+	@Userid varchar(50)
+AS
+	DELETE FROM [UserPasswordHistory] WHERE UserId = @UserId
+GO
