1f326295dbca99b44f246e4b6531d4adef314eb1
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-12-22 15:01:47 +0000

    add FlashMessage view model
    Added the rendering of any info messages to the user in the layout

--- src/SFA.DAS.EmployerUsers.Web/Models/OrchestratorResponse.cs
+++ src/SFA.DAS.EmployerUsers.Web/Models/OrchestratorResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using SFA.DAS.EmployerUsers.Web.Models.SFA.DAS.EAS.Web.Models;
 
 namespace SFA.DAS.EmployerUsers.Web.Models
 {
@@ -11,6 +12,7 @@ namespace SFA.DAS.EmployerUsers.Web.Models
         }
         public HttpStatusCode Status { get; set; }
         public Exception Exception { get; set; }
+        public FlashMessageViewModel FlashMessage { get; set; }
         public string RedirectUrl { get; set; }
         public string RedirectButtonMessage { get; set; }
     }
