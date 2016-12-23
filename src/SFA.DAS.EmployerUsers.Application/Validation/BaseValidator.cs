ac88f63931608de4e885efc0386adceed4e6b351
floydprice <f.price@floydprice.com>
2016-12-23 14:42:32 +0000

    New BaseValidator that has the email regex checker (copied from EmailAddressAttribute)

---
+++ src/SFA.DAS.EmployerUsers.Application/Validation/BaseValidator.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace SFA.DAS.EmployerUsers.Application.Validation
+{
+
+    public class BaseValidator
+    {
+        private static Regex _regex = CreateRegEx();
+
+        public bool IsEmailValid(string emailAddress)
+        {
+            return emailAddress != null && _regex.Match(emailAddress).Length > 0;
+        }
+
+        private static Regex CreateRegEx()
+        {
+
+
+
+            const string pattern = @"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$";
+            const RegexOptions options = RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture;
+
+            // Set explicit regex match timeout, sufficient enough for email parsing
+            // Unless the global REGEX_DEFAULT_MATCH_TIMEOUT is already set
+            TimeSpan matchTimeout = TimeSpan.FromSeconds(2);
+
+            try
+            {
+                if (AppDomain.CurrentDomain.GetData("REGEX_DEFAULT_MATCH_TIMEOUT") == null)
+                {
+                    return new Regex(pattern, options, matchTimeout);
+                }
+            }
+            catch
+            {
+                // Fallback on error
+            }
+
+            // Legacy fallback (without explicit match timeout)
+            return new Regex(pattern, options);
+        }
+    }
+}
--- src/SFA.DAS.EmployerUsers.Web/Models/RequestPasswordResetViewModel.cs
+++ src/SFA.DAS.EmployerUsers.Web/Models/RequestPasswordResetViewModel.cs
@@ -4,8 +4,7 @@ namespace SFA.DAS.EmployerUsers.Web.Models
 {
     public class RequestPasswordResetViewModel : ViewModelBase
     {
-        [EmailAddress]
-        [Required]
+
         public string Email { get; set; }
 
         public bool ResetCodeSent { get; set; }
