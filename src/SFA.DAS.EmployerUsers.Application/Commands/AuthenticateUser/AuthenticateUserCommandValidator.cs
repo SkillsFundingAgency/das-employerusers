cf0b6f9b60704cd59f4a8cb0e4ef62598bd0cec5
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-12-28 10:03:16 +0000

    Implement validator to AuthenticateUserCommand
    Implemented the validator for the AuthenticateUserCommand. This will
    return informative messages based on the command being sent. Email
    validation has also been added.

---
+++ src/SFA.DAS.EmployerUsers.Application/Commands/AuthenticateUser/AuthenticateUserCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using SFA.DAS.EmployerUsers.Application.Validation;
+
+namespace SFA.DAS.EmployerUsers.Application.Commands.AuthenticateUser
+{
+    public class AuthenticateUserCommandValidator : BaseValidator, IValidator<AuthenticateUserCommand>
+    {
+        public Task<ValidationResult> ValidateAsync(AuthenticateUserCommand item)
+        {
+            var validationResult = new ValidationResult();
+
+            if (string.IsNullOrEmpty(item.EmailAddress) || !IsEmailValid(item.EmailAddress))
+            {
+                validationResult.AddError(nameof(item.EmailAddress), "Enter a valid email address");
+            }
+
+            if (string.IsNullOrEmpty(item.Password))
+            {
+                validationResult.AddError(nameof(item.Password), "Enter password");
+            }
+
+            return Task.FromResult(validationResult);
+        }
+    }
+}
