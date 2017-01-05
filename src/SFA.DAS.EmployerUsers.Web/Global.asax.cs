eb4f6d9d53d404fe182cd8276a53a2960a25282f
floydprice <f.price@floydprice.com>
2017-01-05 16:13:06 +0000

    Global exception logging

--- src/SFA.DAS.EmployerUsers.Web/Global.asax.cs
+++ src/SFA.DAS.EmployerUsers.Web/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Azure;
+using NLog;
 
 namespace SFA.DAS.EmployerUsers.Web
 {
@@ -34,6 +35,12 @@ namespace SFA.DAS.EmployerUsers.Web
             {
                 application.Context.Response.Headers.Remove("Server");
             }
+        }
+
+        protected void Application_Error()
+        {
+            var ex = Server.GetLastError();
+            LogManager.GetCurrentClassLogger().Error(ex, $"Application_Error - {ex.Message}");
         }
     }
 }
