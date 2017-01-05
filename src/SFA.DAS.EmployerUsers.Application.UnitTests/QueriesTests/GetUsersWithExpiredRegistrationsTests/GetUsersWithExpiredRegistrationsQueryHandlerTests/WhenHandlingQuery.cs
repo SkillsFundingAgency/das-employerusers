cc3c96d6cd237cdd76b42672ab30f2e0478c4668
Simon Bull <sbulluk@users.noreply.github.com>
2017-01-05 11:59:18 +0000

    Add application query/command to handle removing expired registrations

---
+++ src/SFA.DAS.EmployerUsers.Application.UnitTests/QueriesTests/GetUsersWithExpiredRegistrationsTests/GetUsersWithExpiredRegistrationsQueryHandlerTests/WhenHandlingQuery.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerUsers.Application.Queries.GetUsersWithExpiredRegistrations;
+using SFA.DAS.EmployerUsers.Domain;
+using SFA.DAS.EmployerUsers.Domain.Data;
+
+namespace SFA.DAS.EmployerUsers.Application.UnitTests.QueriesTests.GetUsersWithExpiredRegistrationsTests.GetUsersWithExpiredRegistrationsQueryHandlerTests
+{
+    public class WhenHandlingQuery
+    {
+        private User _user;
+        private Mock<IUserRepository> _userRepository;
+        private GetUsersWithExpiredRegistrationsQueryHandler _handler;
+        private GetUsersWithExpiredRegistrationsQuery _query;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _user = new User
+            {
+                Id = "USER1"
+            };
+
+            _userRepository = new Mock<IUserRepository>();
+            _userRepository.Setup(r => r.GetUsersWithExpiredRegistrations())
+                .ReturnsAsync(new[]
+                {
+                    _user
+                });
+
+            _handler = new GetUsersWithExpiredRegistrationsQueryHandler(_userRepository.Object);
+
+            _query = new GetUsersWithExpiredRegistrationsQuery();
+        }
+
+        [Test]
+        public async Task ThenItShouldReturnArrayOfUsersFromRepo()
+        {
+            // Act
+            var actual = await _handler.Handle(_query);
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(1, actual.Length);
+            Assert.AreSame(_user, actual[0]);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerUsers.Application/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.EmployerUsers.Domain.Data;
+
+namespace SFA.DAS.EmployerUsers.Application.Commands.DeleteUser
+{
+    public class DeleteUserCommandHandler : IAsyncRequestHandler<DeleteUserCommand, Unit>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public DeleteUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteUserCommand message)
+        {
+            await _userRepository.Delete(message.User);
+
+            return Unit.Value;
+        }
+    }
+}
