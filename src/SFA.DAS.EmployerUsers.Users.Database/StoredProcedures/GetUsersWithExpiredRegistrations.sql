e6277f4ab72b33a548e12a46191beba4c7cdd9f0
Simon Bull <sbulluk@users.noreply.github.com>
2017-01-05 11:41:28 +0000

    Add user repository methods for getting users with expired registrations and deleting users

---
+++ src/SFA.DAS.EmployerUsers.Users.Database/StoredProcedures/GetUsersWithExpiredRegistrations.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[GetUsersWithExpiredRegistrations]
+AS
+	SELECT u.Id
+	FROM [User] u
+	LEFT JOIN UserSecurityCode sc
+		ON u.Id = sc.UserId
+		AND sc.ExpiryTime > GETDATE()
+	WHERE u.IsActive = 0
+	AND sc.Code IS NULL
+GO
