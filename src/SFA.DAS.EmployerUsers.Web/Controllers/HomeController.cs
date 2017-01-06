d9f617e0387a63f5191bc9061fd48c723bf595a8
floydprice <f.price@floydprice.com>
2017-01-06 17:55:36 +0000

    More robust redirecting with some logging

--- src/SFA.DAS.EmployerUsers.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.EmployerUsers.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Mvc;
+using NLog;
+using SFA.DAS.EmployerUsers.Infrastructure.Configuration;
 using SFA.DAS.EmployerUsers.Web.Authentication;
 using SFA.DAS.EmployerUsers.WebClientComponents;
 
@@ -7,17 +9,27 @@ namespace SFA.DAS.EmployerUsers.Web.Controllers
     public class HomeController : ControllerBase
     {
         private readonly IOwinWrapper _owinWrapper;
+        private readonly IdentityServerConfiguration _identityServerConfiguration;
+        private readonly ILogger _logger;
 
-        public HomeController(IOwinWrapper owinWrapper)
+        public HomeController(IOwinWrapper owinWrapper, IdentityServerConfiguration identityServerConfiguration, ILogger logger)
         {
             _owinWrapper = owinWrapper;
+            _identityServerConfiguration = identityServerConfiguration;
+            _logger = logger;
         }
 
         public ActionResult Index()
         {
             var returnUrl = _owinWrapper.GetIdsReturnUrl();
-
-            return Redirect(returnUrl);
+            if (!string.IsNullOrEmpty(returnUrl))
+            {
+                Logger.Info($"HomeController:Index - Redirecting user out of iDams to {returnUrl}");
+                return Redirect(returnUrl);
+            }
+            returnUrl = _identityServerConfiguration.EmployerPortalUrl;
+            Logger.Info($"HomeController:Index - Redirecting user out of iDams (brute force) to {returnUrl}");
+            return View(_identityServerConfiguration.EmployerPortalUrl);
         }
 
         public ActionResult CatchAll(string path)
