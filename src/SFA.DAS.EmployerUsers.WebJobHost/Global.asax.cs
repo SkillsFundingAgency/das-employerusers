e4855ca857c1b4b1faede231a726ecd098e262e6
Simon Bull <sbulluk@users.noreply.github.com>
2017-01-06 10:07:56 +0000

    Fix app.config transform issues

--- src/SFA.DAS.EmployerUsers.WebJobHost/Global.asax.cs
+++ src/SFA.DAS.EmployerUsers.WebJobHost/Global.asax.cs
@@ -15,9 +15,6 @@ namespace SFA.DAS.EmployerUsers.WebJobHost
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
-            AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
         }
     }
 }
