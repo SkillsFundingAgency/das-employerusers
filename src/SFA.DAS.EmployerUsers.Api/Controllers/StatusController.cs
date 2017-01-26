450c04c8498907eda422e595aaa9e7d37aee9ad5
floydprice <f.price@floydprice.com>
2017-01-26 18:10:34 +0000

    Status controller implementation

---
+++ src/SFA.DAS.EmployerUsers.Api/Controllers/StatusController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SFA.DAS.EmployerUsers.Api.Controllers
+{
+
+    [RoutePrefix("api/status")]
+    public class StatusController : ApiController
+    {
+        [HttpGet, Route("")]
+        public IHttpActionResult Index()
+        {
+            return Ok();
+        }
+
+        [Route("{random}"), HttpGet]
+        public IHttpActionResult Show(string random)
+        {
+            return Ok(random);
+        }
+    }
+}
