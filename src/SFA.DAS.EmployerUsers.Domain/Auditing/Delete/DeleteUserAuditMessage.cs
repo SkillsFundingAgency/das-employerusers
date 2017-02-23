e3c3ac1860875457fd11d70bc112767fea26123f
Simon Bull <simonbu11@users.noreply.github.com>
2017-02-23 11:27:10 +0000

    Audit when users are removed

---
+++ src/SFA.DAS.EmployerUsers.Domain/Auditing/Delete/DeleteUserAuditMessage.cs
@@ -0,0 +1,18 @@
+﻿using SFA.DAS.Audit.Types;
+
+namespace SFA.DAS.EmployerUsers.Domain.Auditing.Delete
+{
+    public class DeleteUserAuditMessage : EmployerUsersAuditMessage
+    {
+        public DeleteUserAuditMessage(User user)
+        {
+            AffectedEntity = new Entity
+            {
+                Type = UserTypeName,
+                Id = user.Id
+            };
+            Category = "DELETE";
+            Description = $"User {user.Email} (id: {user.Id}) has been deleted as they did not complete their registration";
+        }
+    }
+}
