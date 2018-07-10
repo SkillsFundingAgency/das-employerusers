ed13e78731e5cdb074a923aa0f43f851793be9ec
Iain Stevenson <IainStevenson@live.co.uk>
2018-07-10 11:14:14 +0100

    Certificates upgrade

--- src/DevInstall.ps1
+++ src/DevInstall.ps1
@@ -1,12 +1,6 @@
-<#
-New-SelfSignedCertificate -Subject localhost -CertStoreLocation cert:\LocalMachine\My -DnsName localhost
-#>
-
 #Requires -RunAsAdministrator
 
-$localhostpwd = ConvertTo-SecureString -String password -Force -AsPlainText
-Import-PfxCertificate -FilePath localhost.pfx -CertStoreLocation cert://LocalMachine/My -Password $localhostpwd -Exportable
-Import-PfxCertificate -FilePath SFA.Dev.Local.cer -CertStoreLocation cert://LocalMachine/Root -Password $localhostpwd -Exportable
+$certpwd = ConvertTo-SecureString -String password -Force -AsPlainText
 
-$idppwd = ConvertTo-SecureString -String idsrv3test -Force -AsPlainText
-Import-PfxCertificate -FilePath DasIDPCert.pfx -CertStoreLocation cert://LocalMachine/My -Password $idppwd -Exportable
+Import-PfxCertificate -FilePath $PSScriptRoot\localhost.pfx -CertStoreLocation cert://LocalMachine/My -Password $certpwd -Exportable
+Import-PfxCertificate -FilePath $PSScriptRoot\SFA.Dev.Local.pfx -CertStoreLocation cert://LocalMachine/Root -Password $certpwd -Exportable
