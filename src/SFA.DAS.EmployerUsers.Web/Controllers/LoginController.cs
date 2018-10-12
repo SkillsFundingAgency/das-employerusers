efc96898e3c798219d6615a2b026a4f5117c4126
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-10-12 12:56:16 +0100

    Added redirect that will sort logout issue with new EAS version

--- src/SFA.DAS.EmployerUsers.Web/Controllers/LoginController.cs
+++ src/SFA.DAS.EmployerUsers.Web/Controllers/LoginController.cs
@@ -41,6 +41,17 @@ namespace SFA.DAS.EmployerUsers.Web.Controllers
             var url = Task.Run(async () =>
             {
                 var returnUrl = await _accountOrchestrator.LogoutUrlForClientId(_owinWrapper.GetIdsClientId());
+
+                var easPortalUrl = _identityServerConfiguration.EmployerPortalUrl;
+
+                if (returnUrl.StartsWith(easPortalUrl))
+                {
+                    if (!easPortalUrl.EndsWith("/"))
+                        easPortalUrl += "/";
+
+                    returnUrl = $"{easPortalUrl}service/signOut";
+                }
+
                 return returnUrl;
 
             }).Result;
