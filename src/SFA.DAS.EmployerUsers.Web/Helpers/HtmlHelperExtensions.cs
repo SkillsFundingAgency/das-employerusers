eed7aca3ede16df8ecd3f385270246d359447299
tom-gough <tom@tgsd.co.uk>
2019-10-04 16:16:36 +0100

    zendesk widget and suggestions

---
+++ src/SFA.DAS.EmployerUsers.Web/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace SFA.DAS.EmployerUsers.Web.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString SetZenDeskSuggestion(this HtmlHelper html, string suggestion)
+        {
+            return MvcHtmlString.Create($"<script type=\"text/javascript\">zE('webWidget', 'helpCenter:setSuggestions', {{ search: '{suggestion}' }});</script>");
+        }
+    }
+
+    public static class ZenDeskSuggestions
+    {
+        public static string SetUpAsAUser = "Set-up-as-a-user";
+    }
+}
