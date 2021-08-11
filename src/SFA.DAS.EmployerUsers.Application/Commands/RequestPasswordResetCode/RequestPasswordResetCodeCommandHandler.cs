acc6c711207d2276ca8534950f580e63e1dddc0f
Corey <corey.faulconbridge@digital.education.gov.uk>
2021-08-11 09:19:02 +0100

    Fix issue with static code generator when no previous security code exist

--- src/SFA.DAS.EmployerUsers.Application/Commands/RequestPasswordResetCode/RequestPasswordResetCodeCommandHandler.cs
+++ src/SFA.DAS.EmployerUsers.Application/Commands/RequestPasswordResetCode/RequestPasswordResetCodeCommandHandler.cs
@@ -65,22 +65,25 @@ namespace SFA.DAS.EmployerUsers.Application.Commands.RequestPasswordResetCode
             {
                 if (ConfigurationManager.AppSettings["UseStaticCodeGenerator"].Equals("false", StringComparison.CurrentCultureIgnoreCase))
                 {
-                    existingUser.AddSecurityCode(new SecurityCode
-                    {
-                        Code = _codeGenerator.GenerateAlphaNumeric(),
-                        CodeType = SecurityCodeType.PasswordResetCode,
-                        ExpiryTime = DateTimeProvider.Current.UtcNow.AddDays(1),
-                        ReturnUrl = message.ReturnUrl
-                    });
+                    AddUserSecurityCode(existingUser, message.ReturnUrl);
                 }
                 else
                 {
                     // for static code generator simply set failed attempts back to zero as creating another same security code breaks the unique constraint
-                    existingUser
+                    var firstSecurityCode = existingUser
                         .SecurityCodes
+                        .Where(sc => sc.CodeType == SecurityCodeType.PasswordResetCode)
                         .OrderByDescending(sc => sc.ExpiryTime)
-                        .FirstOrDefault()
-                        .FailedAttempts = 0;
+                        .FirstOrDefault();
+
+                    if(firstSecurityCode == null)
+                    {
+                        AddUserSecurityCode(existingUser, message.ReturnUrl);
+                    }
+                    else
+                    {
+                        firstSecurityCode.FailedAttempts = 0;
+                    }
                 }
 
                 await _userRepository.Update(existingUser);
@@ -101,6 +104,17 @@ namespace SFA.DAS.EmployerUsers.Application.Commands.RequestPasswordResetCode
             return user.SecurityCodes
                 .Where(sc => sc.CodeType == SecurityCodeType.PasswordResetCode)
                 .All(sc => sc.ExpiryTime <= DateTime.UtcNow || sc.FailedAttempts >= 3);
+        }
+
+        private void AddUserSecurityCode(User existingUser, string returnUrl)
+        {
+            existingUser.AddSecurityCode(new SecurityCode
+            {
+                Code = _codeGenerator.GenerateAlphaNumeric(),
+                CodeType = SecurityCodeType.PasswordResetCode,
+                ExpiryTime = DateTimeProvider.Current.UtcNow.AddDays(1),
+                ReturnUrl = returnUrl
+            });
         }
     }
 }
