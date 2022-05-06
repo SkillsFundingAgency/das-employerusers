45dbd91ab58511ed5965975eb48c0b5aff2695df
Chris Woodcock <chris@humesoftware.com>
2022-05-06 10:22:06 +0100

    Removed dependency injection from OwinStartup which does not resolve

--- src/SFA.DAS.EmployerUsers.Support.Web/Startup.cs
+++ src/SFA.DAS.EmployerUsers.Support.Web/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.Owin.Security.ActiveDirectory;
 using Owin;
 using SFA.DAS.NLog.Logger;
 using SFA.DAS.Support.Shared.SiteConnection;
+using System.Web.Mvc;
 
 [assembly: OwinStartup(typeof(SFA.DAS.EmployerUsers.Support.Web.Startup))]
 
@@ -10,28 +11,22 @@ namespace SFA.DAS.EmployerUsers.Support.Web
 {
     public class Startup
     {
-        private readonly ILog _logger;
-        private readonly ISiteValidatorSettings _siteValidatorSettings;
-
-        public Startup(ILog logger, ISiteValidatorSettings siteValidatorSettings)
-        {
-            _logger = logger;
-            _siteValidatorSettings = siteValidatorSettings;
-        }
-
         public void Configuration(IAppBuilder app)
         {
-            _logger.Info($"Configure Authentication Audience:{_siteValidatorSettings.Audience} Tenant:{_siteValidatorSettings.Tenant}");
+            var logger = DependencyResolver.Current.GetService<ILog>();
+            var siteValidatorSettings = DependencyResolver.Current.GetService<ISiteValidatorSettings>();
+            
+            logger.Info($"Configure Authentication Audience:{siteValidatorSettings.Audience} Tenant:{siteValidatorSettings.Tenant}");
 
             app.UseWindowsAzureActiveDirectoryBearerAuthentication(
                new WindowsAzureActiveDirectoryBearerAuthenticationOptions
                {
                    TokenValidationParameters = new System.IdentityModel.Tokens.TokenValidationParameters
                    {
-                       ValidAudiences = _siteValidatorSettings.Audience.Split(','),
+                       ValidAudiences = siteValidatorSettings.Audience.Split(','),
                        RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role"
                    },
-                   Tenant = _siteValidatorSettings.Tenant
+                   Tenant = siteValidatorSettings.Tenant
                });
         }
     }
