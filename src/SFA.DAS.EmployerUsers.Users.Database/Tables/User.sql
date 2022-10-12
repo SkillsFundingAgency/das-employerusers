868e23b210737e895b72bdeefc6b12a6fea2d4aa
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-10-12 18:21:11 +0100

    FAI-630 - Update user table to store gov identifier

--- src/SFA.DAS.EmployerUsers.Users.Database/Tables/User.sql
+++ src/SFA.DAS.EmployerUsers.Users.Database/Tables/User.sql
@@ -9,8 +9,10 @@ CREATE TABLE dbo.[User] (
 	[IsActive]				bit				NOT NULL DEFAULT(1),
 	[FailedLoginAttempts]	int				NOT NULL DEFAULT(0),
 	[IsLocked]				bit				NOT NULL DEFAULT(0),
-	[IsSuspended]			bit				NOT NULL DEFAULT(0)
+	[IsSuspended]			bit				NOT NULL DEFAULT(0),
+    [GovUkIdentifier]       varchar(150)    NULL
 	CONSTRAINT [PK_User] PRIMARY KEY (Id), 
     [LastSuspendedDate] DATETIME NULL,
-	INDEX [IX_User_Email] NONCLUSTERED(Email)
+	INDEX [IX_User_Email] NONCLUSTERED(Email),
+    INDEX [IX_User_GovUkIdentifier] NONCLUSTERED(GovUkIdentifier)
 )
