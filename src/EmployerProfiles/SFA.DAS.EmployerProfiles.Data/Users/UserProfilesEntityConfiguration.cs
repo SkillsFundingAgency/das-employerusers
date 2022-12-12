f4a3acfe3bec2ff00724e1fb7140e95200d5f691
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-12-12 19:59:17 +0000

    FAI-714 - Add entity configuration for User table

---
+++ src/EmployerProfiles/SFA.DAS.EmployerProfiles.Data/Users/UserProfilesEntityConfiguration.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SFA.DAS.EmployerProfiles.Domain.UserProfiles;
+
+namespace SFA.DAS.EmployerProfiles.Data.Users;
+
+public class UserProfilesEntityConfiguration : IEntityTypeConfiguration<UserProfileEntity>
+{
+    public void Configure(EntityTypeBuilder<UserProfileEntity> builder)
+    {
+        builder.ToTable("User");
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.Id).HasColumnName("Id").HasColumnType("uniqueidentifier").IsRequired();
+        builder.Property(x => x.FirstName).HasColumnName("StandardUId").HasColumnType("varchar").HasMaxLength(50).IsRequired(false);
+        builder.Property(x => x.LastName).HasColumnName("StandardUId").HasColumnType("varchar").HasMaxLength(50).IsRequired(false);
+        builder.Property(x => x.Email).HasColumnName("StandardUId").HasColumnType("varchar").HasMaxLength(255).IsRequired();
+        builder.Property(x => x.GovUkIdentifier).HasColumnName("StandardUId").HasColumnType("varchar").HasMaxLength(150).IsRequired(false);
+        
+        builder.HasIndex(x => x.GovUkIdentifier).IsUnique();
+        builder.HasIndex(x => x.Email).IsUnique();
+    }
+}
