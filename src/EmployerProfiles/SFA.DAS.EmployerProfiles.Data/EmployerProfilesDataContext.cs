bb7b266e07ad8d209e3449640bb23a9ed8b0e2d3
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-12-12 19:59:38 +0000

    FAI-714 - Add context and database configuration

---
+++ src/EmployerProfiles/SFA.DAS.EmployerProfiles.Data/EmployerProfilesDataContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SFA.DAS.EmployerProfiles.Domain.Configuration;
+using SFA.DAS.EmployerProfiles.Domain.UserProfiles;
+
+namespace SFA.DAS.EmployerProfiles.Data;
+
+public interface IEmployerProfilesDataContext
+{
+    DbSet<UserProfileEntity> UserProfileEntities { get; set; }
+}
+
+public class EmployerProfilesDataContext : DbContext, IEmployerProfilesDataContext
+{
+    public DbSet<UserProfileEntity> UserProfileEntities { get; set; }
+
+    private readonly EmployerProfilesConfiguration? _configuration;
+
+    public EmployerProfilesDataContext()
+    {
+    }
+
+    public EmployerProfilesDataContext(DbContextOptions options) : base(options)
+    {
+            
+    }
+    public EmployerProfilesDataContext(IOptions<EmployerProfilesConfiguration> config, DbContextOptions options) :base(options)//, AzureServiceTokenProvider azureServiceTokenProvider
+    {
+        _configuration = config.Value;
+        //_azureServiceTokenProvider = azureServiceTokenProvider;
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseLazyLoadingProxies();
+            
+        // if (_configuration == null || _azureServiceTokenProvider == null)
+        // {
+        //     return;
+        // }
+            
+        var connection = new SqlConnection
+        {
+            ConnectionString = _configuration?.ConnectionString,
+     //       AccessToken = _azureServiceTokenProvider.GetAccessTokenAsync(AzureResource).Result,
+        };
+            
+        optionsBuilder.UseSqlServer(connection,options=>
+            options.EnableRetryOnFailure(
+                5,
+                TimeSpan.FromSeconds(20),
+                null
+            ));
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new Users.UserProfilesEntityConfiguration());
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
