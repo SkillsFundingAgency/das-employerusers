942bece51f4395a0fe6e7c1cf7fba5ac7e4c9fe8
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-12-17 13:13:01 +0000

    FAI-714 - Add call to validator for UpsertRequestHandler

--- src/EmployerProfiles/SFA.DAS.EmployerProfiles.Application.UnitTests/Users/Handlers/Commands/WhenHandlingUpsertingUser.cs
+++ src/EmployerProfiles/SFA.DAS.EmployerProfiles.Application.UnitTests/Users/Handlers/Commands/WhenHandlingUpsertingUser.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 using AutoFixture.NUnit3;
 using FluentAssertions;
 using Moq;
 using SFA.DAS.EmployerProfiles.Application.Users.Handlers.Commands.UpsertUser;
+using SFA.DAS.EmployerProfiles.Domain.RequestHandlers;
 using SFA.DAS.EmployerProfiles.Domain.UserProfiles;
 using SFA.DAS.Testing.AutoFixture;
+using ValidationResult = SFA.DAS.EmployerProfiles.Domain.RequestHandlers.ValidationResult;
 
 namespace SFA.DAS.EmployerProfiles.Application.UnitTests.Users.Handlers.Commands;
 
@@ -13,10 +16,12 @@ public class WhenHandlingUpsertingUser
     public async Task Then_The_Repository_Is_Called_To_Upsert_The_UserProfile(
         UserProfileEntity result,
         UpsertUserRequest request,
+        [Frozen] Mock<IValidator<UpsertUserRequest>> validator,
         [Frozen] Mock<IUserProfileRepository> userProfileRepository,
         UpsertUserRequestHandler handler)
     {
         //Arrange
+        validator.Setup(x => x.ValidateAsync(request)).ReturnsAsync(new ValidationResult {ValidationDictionary = { }});
         result.Id = request.Id.ToString();
         userProfileRepository.Setup(x => x.Upsert(
                 It.Is<UserProfileEntity>(c => 
@@ -34,5 +39,21 @@ public class WhenHandlingUpsertingUser
         //Assert
         actual.UserProfile.Should().BeEquivalentTo(result, options=>options.Excluding(c=>c.Id));
         actual.UserProfile.Id.Should().Be(request.Id);
+    }
+
+    [Test, MoqAutoData]
+    public async Task Then_If_Invalid_A_ValidationException_Is_Thrown(
+        UpsertUserRequest request,
+        [Frozen] Mock<IValidator<UpsertUserRequest>> validator,
+        [Frozen] Mock<IUserProfileRepository> userProfileRepository,
+        UpsertUserRequestHandler handler)
+    {
+        //Arrange
+        validator.Setup(x => x.ValidateAsync(request)).ReturnsAsync(new ValidationResult {ValidationDictionary = {{"",""} }});
+        
+        //Act Assert
+        Assert.ThrowsAsync<ValidationException>(()=>handler.Handle(request, CancellationToken.None));
+        userProfileRepository.Verify(x => x.Upsert(
+            It.IsAny<UserProfileEntity>()), Times.Never);
     }
 }
--- src/EmployerProfiles/SFA.DAS.EmployerProfiles.Application/Users/Handlers/Commands/UpsertUser/UpsertUserRequestHandler.cs
+++ src/EmployerProfiles/SFA.DAS.EmployerProfiles.Application/Users/Handlers/Commands/UpsertUser/UpsertUserRequestHandler.cs
@@ -1,4 +1,6 @@
+using System.ComponentModel.DataAnnotations;
 using MediatR;
+using SFA.DAS.EmployerProfiles.Domain.RequestHandlers;
 using SFA.DAS.EmployerProfiles.Domain.UserProfiles;
 
 namespace SFA.DAS.EmployerProfiles.Application.Users.Handlers.Commands.UpsertUser;
@@ -6,13 +8,22 @@ namespace SFA.DAS.EmployerProfiles.Application.Users.Handlers.Commands.UpsertUse
 public class UpsertUserRequestHandler : IRequestHandler<UpsertUserRequest, UpsertUserResult>
 {
     private readonly IUserProfileRepository _userProfileRepository;
+    private readonly IValidator<UpsertUserRequest> _validator;
 
-    public UpsertUserRequestHandler(IUserProfileRepository userProfileRepository)
+    public UpsertUserRequestHandler(IUserProfileRepository userProfileRepository, IValidator<UpsertUserRequest> validator)
     {
         _userProfileRepository = userProfileRepository;
+        _validator = validator;
     }
     public async Task<UpsertUserResult> Handle(UpsertUserRequest request, CancellationToken cancellationToken)
     {
+        var validationResult = await _validator.ValidateAsync(request);
+
+        if (!validationResult.IsValid())
+        {
+            throw new ValidationException(validationResult.DataAnnotationResult,null, null);
+        }
+        
         var result = await _userProfileRepository.Upsert(new UserProfileEntity
         {
             Id = request.Id.ToString(),
