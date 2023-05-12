0b556e232c8da1537aeff7313383a6ba1791751f
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2023-05-12 08:25:30 +0100

    FAI-804 - Set first name and last name to be nullable

--- src/EmployerProfiles/SFA.DAS.EmployerProfiles.Api/ApiRequests/UserProfileRequest.cs
+++ src/EmployerProfiles/SFA.DAS.EmployerProfiles.Api/ApiRequests/UserProfileRequest.cs
@@ -1,18 +1,13 @@
 using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Mvc;
 
-namespace SFA.DAS.EmployerProfiles.Api.ApiRequests;
+    namespace SFA.DAS.EmployerProfiles.Api.ApiRequests;
 
 public class UserProfileRequest
 {
-    [FromQuery]
     [Required]
     public string Email { get; set; }
-    [FromQuery]
-    public string FirstName { get; set; }
-    [FromQuery]
-    public string LastName { get; set; }
-    [FromQuery]
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     [Required]
     public string GovIdentifier { get; set; }
 }
