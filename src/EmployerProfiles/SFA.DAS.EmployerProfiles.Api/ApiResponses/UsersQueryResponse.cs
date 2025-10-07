51e9c8ae3085a2405d681e0020218feeb3629df3
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-10-07 11:42:59 +0100

    Clean up

--- src/EmployerProfiles/SFA.DAS.EmployerProfiles.Api/ApiResponses/UsersQueryResponse.cs
+++
@@ -1,8 +0,0 @@
-using SFA.DAS.EmployerProfiles.Domain.UserProfiles;
-
-namespace SFA.DAS.EmployerProfiles.Api.ApiResponses;
-
-public class UsersQueryResponse
-{
-    public List<UserProfile> Users { get; set; } = new ();
-}
